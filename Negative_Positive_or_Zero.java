c2e92ec3ea6e0e436c5703dfea7418140ea3deba
Deepak Dongare <52232024+Deepak-Dongare@users.noreply.github.com>
2019-08-08 12:52:00 -0700

    Negative_Positive_or_Zero_java

---
+++ Negative_Positive_or_Zero.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Negative_Positive_or_Zero {
+	public static void main(String[] args) {
+		int b=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any Number");
+		int a=sc.nextInt();
+		if(b>a)
+		{
+			System.out.println("Number is Negative");
+		}
+		if(b<a)
+		{
+			System.out.println("Number is Postive");
+		}
+		if(a==b) {
+			System.out.println("number is zero");
+		}
+		else
+		{
+			System.out.println("Enter number");
+		}
+				
+	}
+
+}
